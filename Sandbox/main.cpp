7f9e6715583efedc626e9125d1c844614930b753
quang <quang@PRO-DEV>
2026-03-19 22:36:57 +0700

    test 1

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -3,6 +3,24 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    TestTexture();
+    GameApp app(1920, 1080, "My Game");
+
+    auto anim = new Animation2DComponent();
+
+	auto player = new Player(); 
+
+    auto tilemap = new TiltedMapComponent("assets/demo.tmx");
+	auto sprite = new SpriteComponent(new Texture("assets/images/tilemap/Run.png"));
+    
+	sprite->x = 0;
+	sprite->y = 0;
+
+    app.Add(tilemap);
+	app.Add(sprite);
+
+    app.Add(player);
+
+    app.Run();
+    //TestTexture();
     return 0;
 }
