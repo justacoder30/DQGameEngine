7beadd3dcb01492ec0b0088fc607c2cf73fae73c
quang <quang@PRO-DEV>
2026-03-20 02:26:45 +0700

    add bg to test

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -11,10 +11,15 @@ int main(int argc, char** argv) {
 
     auto tilemap = new TiltedMapComponent("assets/demo.tmx");
 	auto sprite = new SpriteComponent(new Texture("assets/images/tilemap/Run.png"));
+    auto bg = new SpriteComponent(new Texture("resource/img/Background/background.png"));
+
+	bg->width = 1920;   
+	bg->height = 1080;
     
 	sprite->x = 0;
 	sprite->y = 0;
 
+	app.Add(bg);
     app.Add(tilemap);
 	app.Add(sprite);
 
